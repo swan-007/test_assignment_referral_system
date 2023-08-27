1286ce04b609fd9ccff71f832ea319fb0c2ab688
Sergey Lebedev <superlebedew@gmail.com>
2023-08-27 22:18:32 +0300

    add_new_postman_collection

--- referral_system/referral_system/urls.py
+++ referral_system/referral_system/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path("api/v1/", include("backend.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="docs"),
+
 ]
